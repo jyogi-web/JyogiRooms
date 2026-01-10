f0cbc5445dc5dbfa1a0491c60784404025fab105
RiTa-23 <rita.tifrt@gmail.com>
2026-01-10 12:17:48 +0900

    chore:テスト再現性向上のため、投入データの時間を秒単位(00秒)まで固定

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,22 +19,22 @@ now = Time.current
 # 1. Past Reservation (Yesterday)
 Reservation.create!(
   user: user1,
-  start_at: now.yesterday.change(hour: 10, min: 0),
-  end_at: now.yesterday.change(hour: 12, min: 0)
+  start_at: now.yesterday.change(hour: 10, min: 0, sec: 0),
+  end_at: now.yesterday.change(hour: 12, min: 0, sec: 0)
 )
 
 # 2. Today's Reservation
 Reservation.create!(
   user: user2,
-  start_at: now.change(hour: 13, min: 0),
-  end_at: now.change(hour: 15, min: 0)
+  start_at: now.change(hour: 13, min: 0, sec: 0),
+  end_at: now.change(hour: 15, min: 0, sec: 0)
 )
 
 # 3. Future Reservation (Tomorrow)
 Reservation.create!(
   user: user1,
-  start_at: now.tomorrow.change(hour: 10, min: 0),
-  end_at: now.tomorrow.change(hour: 12, min: 0)
+  start_at: now.tomorrow.change(hour: 10, min: 0, sec: 0),
+  end_at: now.tomorrow.change(hour: 12, min: 0, sec: 0)
 )
 
 puts "Done! Created #{User.count} users and #{Reservation.count} reservations."
