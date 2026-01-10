e662e20214297dc86ea31a8d9e8fc1197bc7b918
unknown <0gy27b297263j6s@au.com>
2026-01-10 18:19:11 +0900

    rabbitの修正

--- discord-bot/src/index.js
+++ discord-bot/src/index.js
@@ -13,4 +13,9 @@ client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.login(process.env.DISCORD_BOT_TOKEN);
+client.login(process.env.DISCORD_BOT_TOKEN)
+  .catch((error) => {
+    console.error('Failed to login to Discord:', error);
+    console.error('DISCORD_BOT_TOKEN is', process.env.DISCORD_BOT_TOKEN ? 'present' : 'missing');
+    process.exit(1);
+  });
