34e180586469865f699dbf4f15ab08716b3798ad
NazonoKansatugata <0gy27b297263j6s@au.com>
2026-01-11 02:13:22 +0900

    Refactor key_transfer_logs migration

--- db/migrate/20260110032000_create_key_transfer_logs.rb
+++ db/migrate/20260110032000_create_key_transfer_logs.rb
@@ -1,18 +1,12 @@
 class CreateKeyTransferLogs < ActiveRecord::Migration[8.1]
   def change
     create_table :key_transfer_logs do |t|
-      t.bigint :from_user_id, null: false
-      t.bigint :to_user_id, null: false
-      t.bigint :room_id, null: false
+      t.references :from_user, null: false, foreign_key: { to_table: :users }, index: true
+      t.references :to_user, null: false, foreign_key: { to_table: :users }, index: true
+      t.references :room, null: false, foreign_key: true, index: true
       t.timestamps
     end
-    add_foreign_key :key_transfer_logs, :users, column: :from_user_id
-    add_foreign_key :key_transfer_logs, :users, column: :to_user_id
-    add_foreign_key :key_transfer_logs, :rooms
 
-    add_index :key_transfer_logs, :from_user_id
-    add_index :key_transfer_logs, :to_user_id
-    add_index :key_transfer_logs, :room_id
     add_index :key_transfer_logs, :created_at
     add_index :key_transfer_logs, %i[room_id created_at]
   end
