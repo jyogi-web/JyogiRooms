938fb402e395977988ba4b47a588085dbdcf9425
RiTa-23 <rita.tifrt@gmail.com>
2026-01-12 18:02:28 +0900

    fix:lintエラー修正

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -1,6 +1,6 @@
 module ReservationsHelper
   def format_date_with_weekday(date)
     return "" if date.nil?
-    "#{date.strftime('%Y年%m月%d日')} (#{%w(日 月 火 水 木 金 土)[date.wday]})"
+    "#{date.strftime('%Y年%m月%d日')} (#{%w[日 月 火 水 木 金 土][date.wday]})"
   end
 end
