158825aa6777695b28bf93711c21580126b85c5f
RiTa-23 <rita.tifrt@gmail.com>
2026-01-13 16:17:32 +0900

    fix: ReservationsHelperのシンタックスエラー修正（不要なend削除）

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -3,7 +3,7 @@ module ReservationsHelper
     return "" if date.nil?
     "#{date.strftime('%Y年%m月%d日')} (#{%w[日 月 火 水 木 金 土][date.wday]})"
   end
-  end
+
 
   def format_time_short(time)
     return "" if time.nil?
