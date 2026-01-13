d7d6d96e1e8815949032c4501c384dcb395e90af
RiTa-23 <rita.tifrt@gmail.com>
2026-01-13 16:03:42 +0900

    refactor: 時刻フォーマットをReservationsHelperに共通化とDayComponentへの適用

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -3,4 +3,10 @@ module ReservationsHelper
     return "" if date.nil?
     "#{date.strftime('%Y年%m月%d日')} (#{%w[日 月 火 水 木 金 土][date.wday]})"
   end
+  end
+
+  def format_time_short(time)
+    return "" if time.nil?
+    time.strftime("%H:%M")
+  end
 end
