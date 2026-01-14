cd9b29da9aab4e2460d112cf0b535e3dee4bfdf8
NazonoKansatugata <0gy27b297263j6s@au.com>
2026-01-15 01:56:08 +0900

    fix ラビット君の編集

--- app/controllers/keys_controller.rb
+++ app/controllers/keys_controller.rb
@@ -1,10 +1,10 @@
 class KeysController < ApplicationController
-  # current_user は利用可能前提
+  before_action :authenticate_user!, only: [:update]
 
   # GET /rooms/:room_id/key
   # 現在の鍵持ちを取得
   def show
-     users = KeyService.current_holders(params[:room_id])
+    users = KeyService.current_holders(params[:room_id])
 
     render json: {
       room_id: params[:room_id],
@@ -24,7 +24,7 @@ class KeysController < ApplicationController
       room_id: params[:room_id],
       from_user: current_user,
       to_user_id: params[:to_user_id]
-    ).call
+    )
 
     render json: { status: "ok" }
   end
