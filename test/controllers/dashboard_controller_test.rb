270b1b5b43b38982e1160204e8c7884d918742a5
RiTa-23 <rita.tifrt@gmail.com>
2026-01-15 15:39:43 +0900

    fix:フィクスチャの使用

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -2,12 +2,7 @@ require "test_helper"
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @user = User.create!(
-      username: "test_user",
-      display_name: "Test User",
-      discord_id: "test_discord_id",
-      jyogi_user_id: SecureRandom.uuid
-    )
+    @user = users(:one)
   end
 
   test "should get index" do
