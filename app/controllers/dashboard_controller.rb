49eaeb654ab9375693e7cb683dfcfeab6d7cef76
NazonoKansatugata <0gy27b297263j6s@au.com>
2026-01-17 10:09:32 +0900

    feat 第三部室から表示されてしまう問題を解決

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def index
     @reservations = Reservation.where(start_at: Time.zone.now.all_day).includes(:user).order(:start_at)
-    @rooms = Room.includes(keys: :user).order(:room_number)
+    @rooms = Room.includes(keys: :user).order(room_number: :desc)
   end
 end
