fbd50089ab7f280ffd34282f398b02668b13df63
RiTa-23 <rita.tifrt@gmail.com>
2026-01-17 16:03:34 +0900

    fix:showへのルートを除外

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "dashboard#index"
-  resources :reservations
+  resources :reservations, except: [:show]
   resources :keys, only: [ :index ]
 
   resources :rooms, only: [] do
