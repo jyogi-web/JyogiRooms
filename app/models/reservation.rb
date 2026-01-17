e2525f31808bed69560d246704526f8418fc5efd
RiTa-23 <rita.tifrt@gmail.com>
2026-01-17 17:38:00 +0900

    feature:予約エラーの内容を表示,railsのエラー画面に飛ばないようにした

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,8 +1,8 @@
 class Reservation < ApplicationRecord
   belongs_to :user
 
-  validates :start_at, presence: true
-  validates :end_at, presence: true
+  validates :start_time, presence: { message: "を入力してください" }
+  validates :end_time, presence: { message: "を入力してください" }
   validate :start_at_cannot_be_in_the_past
   validate :end_at_must_be_after_start_at
   validate :cannot_overlap_with_others
@@ -23,21 +23,21 @@ class Reservation < ApplicationRecord
     begin
       date = date_val.is_a?(Date) ? date_val : Date.parse(date_val.to_s)
     rescue ArgumentError
-      errors.add(:reservation_date, "invalid format")
+      errors.add(:reservation_date, "日付形式が正しくありません")
       return
     end
 
     begin
       s_time = Time.parse(start_time.to_s)
     rescue ArgumentError
-      errors.add(:start_time, "invalid format")
+      errors.add(:start_time, "形式が正しくありません")
       return
     end
 
     begin
       e_time = Time.parse(end_time.to_s)
     rescue ArgumentError
-      errors.add(:end_time, "invalid format")
+      errors.add(:end_time, "形式が正しくありません")
       return
     end
 
@@ -50,7 +50,7 @@ class Reservation < ApplicationRecord
     return if start_at.blank? || end_at.blank?
 
     if end_at <= start_at
-      errors.add(:end_at, "must be after the start time")
+      errors.add(:end_time, "開始時刻より先の時刻を指定してください")
     end
   end
 
@@ -65,7 +65,7 @@ class Reservation < ApplicationRecord
                            .exists?
 
     if existing_reservation
-      errors.add(:base, "This time slot is already booked")
+      errors.add(:base, "指定された時間は既に予約が入っています")
     end
   end
 
@@ -73,7 +73,7 @@ class Reservation < ApplicationRecord
     return if start_at.blank?
 
     if start_at < Time.current
-      errors.add(:start_at, "can't be in the past")
+      errors.add(:start_time, "現在時刻より先の時刻を指定してください")
     end
   end
 end
