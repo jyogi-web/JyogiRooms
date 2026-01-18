ca0707fbc9e4aa2e017f8d7db2bc208d39867e78
NazonoKansatugata <0gy27b297263j6s@au.com>
2026-01-18 13:32:38 +0900

    fix

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,7 +5,7 @@ class Admin::UsersController < Admin::BaseController
   end
 
   def update
-    @user = User.find(params[:id])
+    @user = User.includes(:role, :access_tokens, :reservations, :keys).find(params[:id])
     if @user.update(user_params)
       redirect_to admin_user_path(@user), notice: "ユーザー「#{@user.display_name}」を更新しました"
     else
