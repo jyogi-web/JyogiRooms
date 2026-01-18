623ce9c9f3a99e1b1ef88a484557cac405ae1d05
NazonoKansatugata <0gy27b297263j6s@au.com>
2026-01-18 14:01:12 +0900

    fix インデックスレベルでの指定のみを保持

--- db/migrate/20260117093000_create_roles_and_add_role_to_users.rb
+++ db/migrate/20260117093000_create_roles_and_add_role_to_users.rb
@@ -1,11 +1,11 @@
 class CreateRolesAndAddRoleToUsers < ActiveRecord::Migration[8.1]
   def change
     create_table :roles do |t|
-      t.string :name, null: false, unique: true
+      t.string :name, null: false
 
       t.timestamps
+      t.index :name, unique: true
     end
-    add_index :roles, :name, unique: true
 
     add_reference :users, :role, foreign_key: { to_table: :roles }
   end
