a7b9466a5bfff3a57f32e464484769f4a9da78e3
RiTa-23 <rita.tifrt@gmail.com>
2026-03-25 18:28:18 +0900

    feat: 施錠アナウンスの編集機能を改善

--- app/controllers/admin/scheduled_announcements_controller.rb
+++ app/controllers/admin/scheduled_announcements_controller.rb
@@ -1,20 +1,23 @@
 # frozen_string_literal: true
 
 class Admin::ScheduledAnnouncementsController < Admin::BaseController
-  def edit
-    @announcement = ScheduledAnnouncement.current
-  end
+  before_action :set_announcement
+
+  def edit; end
 
   def update
-    @announcement = ScheduledAnnouncement.current
     if @announcement.update(announcement_params)
-      redirect_to edit_admin_scheduled_announcement_path(@announcement), notice: "施錠アナウンス設定を更新しました"
+      redirect_to edit_admin_scheduled_announcement_path, notice: "施錠アナウンス設定を更新しました"
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
   private
+
+  def set_announcement
+    @announcement = ScheduledAnnouncement.current
+  end
 
   def announcement_params
     params.require(:scheduled_announcement).permit(:message, :hour, :minute, :enabled)
