4a50d8b9a1e62d5677c3ea3b59ab8257897e76c0
RiTa-23 <rita.tifrt@gmail.com>
2026-03-27 19:31:37 +0900

    fix: 不要なコマンドを削除し、ヘルプを更新

--- discord-bot/src/commands/index.ts
+++ discord-bot/src/commands/index.ts
@@ -4,10 +4,6 @@ import { createCommand } from './create.js';
 import { keyCommand } from './key.js';
 import { callCommand } from './call.js';
 import { helpCommand } from './help.js';
-import { openCommand } from './open.js';
-import { closeCommand } from './close.js';
-import { enterCommand } from './enter.js';
-import { exitCommand } from './exit.js';
 import { statusCommand } from './status.js';
 
 export const commands = [
@@ -17,9 +13,5 @@ export const commands = [
     keyCommand,
     callCommand,
     helpCommand,
-    openCommand,
-    closeCommand,
-    enterCommand,
-    exitCommand,
     statusCommand,
 ];
