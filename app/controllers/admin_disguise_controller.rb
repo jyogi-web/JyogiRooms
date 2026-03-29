6c185a37d3b645ba88050fe3247f0e2c63f6d4ef
RiTa-23 <rita.tifrt@gmail.com>
2026-03-29 12:15:47 +0900

    feat: 偽装機能のトグル時にリダイレクト処理を修正

--- app/controllers/admin_disguise_controller.rb
+++ app/controllers/admin_disguise_controller.rb
@@ -6,7 +6,13 @@ class AdminDisguiseController < ApplicationController
   # POST /admin_disguise/toggle
   def toggle
     session[:admin_disguise] = !session[:admin_disguise]
-    redirect_back fallback_location: root_path
+
+    if session[:admin_disguise]
+      # 偽装ON: 管理画面にいると権限エラーになるためルートに遷移
+      redirect_to root_path
+    else
+      redirect_back fallback_location: root_path
+    end
   end
 
   private
