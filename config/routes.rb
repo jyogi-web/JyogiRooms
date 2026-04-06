8ee5553d34801cc7d6b4286fa347e07b7187d39a
RiTa-23 <rita.tifrt@gmail.com>
2026-04-07 02:48:10 +0900

    feat: NFCカード情報の表示機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -83,6 +83,7 @@ Rails.application.routes.draw do
     root "roles#index"
     resources :roles, only: %i[index show update]
     resources :users, only: %i[show update]
+    resources :nfc_cards, only: %i[index]
     resources :key_transfer_logs, only: %i[index show]
     resource :scheduled_announcement, only: %i[edit update]
   end
