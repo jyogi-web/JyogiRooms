a68fffe35af7b1834e32078e3f25f8ac4a8c39fe
RiTa-23 <rita.tifrt@gmail.com>
2026-04-07 03:58:11 +0900

    feat: 日時表示を日本時間にフォーマットするヘルパーを追加

--- app/models/app_update.rb
+++ app/models/app_update.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class AppUpdate < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 255 }
   validates :description, presence: true
   validates :released_on, presence: true
 end
